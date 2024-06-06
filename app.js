0087821d35387861fcfc569b8ae5f05ad605fdd9
Djekids <djekids1984@gmail.com>
2024-06-06 23:12:23 +0300

    Очередной коммит

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-console.log ('Простой вывод текста из моего первого скрипта');
-
 let textTest = ('Результат вычислений');
+const prov = textTest;
 let b = 2;
 let x = 10;
 let y =20;
 let a = (x+y)/b;
-console.log (textTest + ' = '+ a);
+alert (prov + ' = '+ a);
+console.log (prov + ' = '+ a + (' (из скрипта в консоль)'));
