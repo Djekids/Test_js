e135e92b5f0534c58c14126f89b789aa38c113df
Djekids <djekids1984@gmail.com>
2024-06-06 22:56:42 +0300

    Первые вычисления

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-console.log ('Первый commit из тела скрипта');
-console.log ('Втрой commit из тела скрипта');
-console.log ('Третий commit из тела скрипта');
-let textTest = ('это текст');
-console.log (textTest);
+console.log ('Текст из тела скрипта');
 
-
+let textTest = ('Результат вычислений');
+let x = 10;
+let y =20;
+let a = (x+y)/2;
+console.log (textTest + ' = '+ a);
