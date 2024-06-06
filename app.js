a23e5ac26dc67d4e2ee2b582835cae87c0223362
Djekids <djekids1984@gmail.com>
2024-06-06 22:48:01 +0300

    Фиксация из VS

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-console.log ('Первый commit');
+console.log ('Первый commit из тела скрипта');
 console.log ('Втрой commit из тела скрипта');
 console.log ('Третий commit из тела скрипта');
-
 let textTest = ('это текст');
-
-
-
 console.log (textTest);
 
 
