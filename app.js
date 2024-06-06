807acec2cce09e4df4b4ff5d8edb3f4a6fd8e163
Djekids <djekids1984@gmail.com>
2024-06-06 23:02:38 +0300

    Мелкая правка
    заменил число деления на переменную

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 console.log ('Простой вывод текста из моего первого скрипта');
 
 let textTest = ('Результат вычислений');
+let b = 2;
 let x = 10;
 let y =20;
-let a = (x+y)/2;
+let a = (x+y)/b;
 console.log (textTest + ' = '+ a);
