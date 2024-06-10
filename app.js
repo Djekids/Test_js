a7bd0461193723712d944e8add0fdfb207e5d913
Djekids <djekids1984@gmail.com>
2024-06-10 07:42:12 +0300

    операторы сравнения

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ let b = 2;
 let x = 10;
 let y =20;
 let a = (x+y)/b;
-alert (prov + ' = '+ a);
-console.log (prov + ' = '+ a + (' (из скрипта в консоль)'));
+//alert (prov + ' = '+ a);
+//console.log (prov + ' = '+ a + (' (из скрипта в консоль)'));
 /*
 Это
 многосточный
@@ -15,12 +15,31 @@ console.log (prov + ' = '+ a + (' (из скрипта в консоль)'));
 */
 let z=10, c=20, v=30, n=40;
 let m = (z+c+v+n);
-alert ('Результат сложения переменных в упрощенном виде = ' + (z+c+v+n));
+//alert ('Результат сложения переменных в упрощенном виде = ' + (z+c+v+n));
 let s, d, f, g, h;
 s = 10;
 d = 20;
 f = 30;
 v = 20;
 h = 20;
-alert (s+d+f+v+h);
+//alert (s+d+f+v+h);
+
+// Операторы присваивания
+let age = 18+5;
+age += 5;
+age -= 10;
+age *= 2;
+//age /= 3;
+age ++; //увеличение на еденицу
+age --; // уменьшение на еденицу
+console.log(age);
+
+// Операторы сравнения
+const vasya = 20;
+
+console.log(age < vasya); // age меньше вася
+console.log(age <= vasya); // age меньше или равно вася
+console.log(age == vasya); // age равно вася
+console.log(age > vasya); // age больше вася
+console.log(age >= vasya); // age больше или равно вася
 
